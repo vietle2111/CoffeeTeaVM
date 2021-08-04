4366b27aef3c15d7857816a8446ff06d3a4e9057
Tia Karia <tkaria14@gmail.com>
2021-08-04 14:31:54 -0400

    ui folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UiComponent } from './ui/ui.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UiComponent
   ],
   imports: [
     BrowserModule,
