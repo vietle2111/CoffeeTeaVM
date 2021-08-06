76a1ab1e5c6db8501ab7d9cbed453d78727057b4
VIET LE <vietle211@gmail.com>
2021-08-06 01:51:09 -0400

    Fix and change components: choose-drink, report-option, container-status

--- src/app/ui/report-option/report-option.component.ts
+++ src/app/ui/report-option/report-option.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Container } from 'src/app/models/container';
+import { ContainerService } from 'src/app/services/container.service';
 
 @Component({
   selector: 'app-report-option',
@@ -7,9 +9,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReportOptionComponent implements OnInit {
 
-  constructor() { }
+  maxCtn: Container;
+  availableCtn: Container;
+  constructor(private cs:ContainerService) { }
 
   ngOnInit(): void {
+    this.cs.getMaxContainerValue().subscribe(c => {this.maxCtn=c});
+    this.cs.getAvailableContainerValue().subscribe(c => {this.availableCtn=c});
   }
 
 }
