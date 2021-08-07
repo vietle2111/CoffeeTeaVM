8f6b506fdf0c969eba17c24708b4486e45b89632
VIET LE <vietle211@gmail.com>
2021-08-07 03:46:13 -0400

    Complete Refilling counter details

--- src/app/models/refill.ts
+++ src/app/models/refill.ts
@@ -1,9 +1,9 @@
 export class Refill {
     refillId: number;
-    teaRefill:number;
-    coffeeRefill: number;
-    sugarRefill: number;
-    waterRefill: number;
-    milkRefill: number;
+    teaRefill: number = 0;
+    coffeeRefill: number = 0;
+    sugarRefill: number = 0;
+    waterRefill: number = 0;
+    milkRefill: number = 0;
     date: Date;
 }
--- src/app/ui/refilling-detail/refilling-detail.component.ts
+++ src/app/ui/refilling-detail/refilling-detail.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { shareReplay } from 'rxjs/operators';
+import { Refill } from 'src/app/models/refill';
+import { RefillService } from 'src/app/services/refill.service';
 
 @Component({
   selector: 'app-refilling-detail',
@@ -6,10 +9,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./refilling-detail.component.css']
 })
 export class RefillingDetailComponent implements OnInit {
+  allRf = new Refill;
 
-  constructor() { }
+  constructor(private rs:RefillService) { }
 
   ngOnInit(): void {
+    this.allRf = new Refill;
+    this.rs.getRefillList().subscribe(rf => {
+      rf.forEach(sr => {
+        this.allRf.teaRefill += (sr.teaRefill!=0 ? 1 : 0);
+        this.allRf.coffeeRefill += (sr.coffeeRefill!=0 ? 1 : 0);
+        this.allRf.milkRefill += (sr.milkRefill!=0 ? 1 : 0);
+        this.allRf.sugarRefill += (sr.sugarRefill!=0 ? 1 : 0);
+        this.allRf.waterRefill += (sr.waterRefill!=0 ? 1 : 0);
+      })
+    })
   }
 
 }
