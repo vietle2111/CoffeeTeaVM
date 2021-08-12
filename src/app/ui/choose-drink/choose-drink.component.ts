c543ed28963861df8c301e1bf305409a08e97c25
VIET LE <vietle211@gmail.com>
2021-08-12 16:01:47 -0400

    Add alert message for success and fail cases

--- src/app/ui/choose-drink/choose-drink.component.ts
+++ src/app/ui/choose-drink/choose-drink.component.ts
@@ -14,12 +14,12 @@ import { Container } from 'src/app/models/container';
 })
 export class ChooseDrinkComponent implements OnInit {
   drinkList: Drink[] = [];
-  dates:string = (new Date).toISOString();
+  //dates:string = (new Date).toISOString();
   msg:string = "";
   numberOfCup:number=0;
   newCtnValue = new Container;
   isEnough = false;
-    
+  alert_type = "alert-light";  
 
   constructor(private cs:ContainerService, private ds: DrinkService, private route:ActivatedRoute, private sls:SaleListService ) { }
 
@@ -28,28 +28,40 @@ export class ChooseDrinkComponent implements OnInit {
   }
 
   addSaleList(dr:Drink){
+    //this.numberOfCup = Number(document.getElementById(`numberOfCup_${dr.id}`).innerText);
+    this.numberOfCup=Number((<HTMLInputElement>document.getElementById(`numberOfCup_${dr.id}`)).value);
     let noc = this.numberOfCup;
-    this.cs.getAvailableContainerValue().subscribe(rs => {
-    this.newCtnValue.teaContainer = rs.teaContainer - dr.tea*noc/1000;
-    this.newCtnValue.coffeeContainer = rs.coffeeContainer - dr.coffee*noc/1000;
-    this.newCtnValue.milkContainer = rs.milkContainer - dr.milk*noc/1000;
-    this.newCtnValue.sugarContainer = rs.sugarContainer - dr.sugar*noc/1000;
-    this.newCtnValue.waterContainer = rs.waterContainer - dr.water*noc/1000;
-    //check if containers are enough for making drinks
-    if (this.newCtnValue.teaContainer<=0 || this.newCtnValue.coffeeContainer<=0 ||
-      this.newCtnValue.milkContainer<=0 || this.newCtnValue.sugarContainer<=0 ||
-      this.newCtnValue.waterContainer<=0)
-      this.msg = "Containers are not enough. Please refill";
-    else{
-      // update containers with new avalue
-      this.cs.updateCurrentContainer(this.newCtnValue).subscribe(rs => {this.msg += " Containers are updated"});
-      // add sales into salelist
-      this.sls.addSaleList(this.setSaleListValue(dr,noc)).subscribe(rs =>{ 
-        if (rs.saleListId!=0){
-          this.msg = `${this.numberOfCup} ${dr.name}(s) have been made.`;
-        }});
+    if (noc==0) {
+      this.alert_type = "alert-warning";
+      this.msg = `Please enter a number of cups for ${dr.name}.`;
     }
-    });
+    else{
+      this.cs.getAvailableContainerValue().subscribe(rs => {
+        this.newCtnValue.teaContainer = rs.teaContainer - dr.tea*noc/1000;
+        this.newCtnValue.coffeeContainer = rs.coffeeContainer - dr.coffee*noc/1000;
+        this.newCtnValue.milkContainer = rs.milkContainer - dr.milk*noc/1000;
+        this.newCtnValue.sugarContainer = rs.sugarContainer - dr.sugar*noc/1000;
+        this.newCtnValue.waterContainer = rs.waterContainer - dr.water*noc/1000;
+        //check if containers are enough for making drinks
+        if (this.newCtnValue.teaContainer<=0 || this.newCtnValue.coffeeContainer<=0 ||
+          this.newCtnValue.milkContainer<=0 || this.newCtnValue.sugarContainer<=0 ||
+          this.newCtnValue.waterContainer<=0){
+          this.alert_type = "alert-warning";
+          this.msg = "Containers are not enough. Please refill!";
+        }
+        else{
+          // update containers with new avalue
+          this.cs.updateCurrentContainer(this.newCtnValue).subscribe(rs => {});
+          // add sales into salelist
+          this.sls.addSaleList(this.setSaleListValue(dr,noc)).subscribe(rs =>{ 
+            if (rs.saleListId!=0){
+              this.msg = `${this.numberOfCup} ${dr.name}(s) have been made.`;
+            }});
+          this.alert_type = "alert-success";
+        }
+      });
+    }
+    document.getElementById("alert_msg").style.visibility = "visible";
   }
 
   setSaleListValue(dr: Drink, noc: number):string{
@@ -58,5 +70,9 @@ export class ChooseDrinkComponent implements OnInit {
     newSale.date = new Date;
     newSale.numberOfCup = noc;
     return JSON.stringify(newSale);
+  }
+
+  hideAlert(){
+    document.getElementById("alert_msg").style.visibility="hidden";
   }
 }
